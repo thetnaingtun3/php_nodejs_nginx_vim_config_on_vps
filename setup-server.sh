3ae142207299cb37f05e63225da11e67fb1ffcab
Thet Naing Tun <56593582+thetnaingtun3@users.noreply.github.com>
2025-04-08 13:33:51 +0630

    add php Extensions

--- setup-server.sh
+++ setup-server.sh
@@ -28,7 +28,18 @@ fi
 
 # Install PHP and modules
 echo "Installing PHP $php_version..."
-sudo apt-get install -y php$php_version php$php_version-fpm php$php_version-cli php$php_version-mysql php$php_version-curl php$php_version-xml
+#sudo apt-get install -y php$php_version php$php_version-fpm php$php_version-cli php$php_version-mysql php$php_version-curl php$php_version-xml
+
+
+sudo apt install -y \
+    php$php_version php$php_version-cli php$php_version-common php$php_version-curl \
+    php$php_version-gd php$php_version-intl php$php_version-mbstring php$php_version-mysql \
+    php$php_version-opcache php$php_version-readline php$php_version-xml php$php_version-xsl \
+    php$php_version-zip php$php_version-bz2 php$php_version-fpm php$php_version-sqlite3 \
+    php$php_version-imagick php$php_version-bcmath php$php_version-soap php$php_version-igbinary \
+    php$php_version-redis php$php_version-memcached php$php_version-apcu php$php_version-mongodb \
+    php$php_version-memcache php$php_version-pgsql php$php_version-sodium php$php_version-geoip \
+    php$php_version-ldap php$php_version-swoole php$php_version-openssl
 
 # Add Node.js repository and install Node.js 20
 echo "Adding Node.js 20 repository..."
